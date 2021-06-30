66cd420d7f46a02abf2b7bd22fa1f19d60588899
Alejahndro <alejahndro02@gmail.com>
2021-06-30 12:38:59 -0500

    Se importo el modulo MatTableDataModule y se declaro el metodo applyFilter para poder filtar los datos de la tabla

--- src/app/components/list-empleado/list-empleado.component.ts
+++ src/app/components/list-empleado/list-empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
   name: string;
@@ -26,10 +27,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ListEmpleadoComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
