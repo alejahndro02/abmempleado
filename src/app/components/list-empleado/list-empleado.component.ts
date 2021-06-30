72073727bd353632e2702ca74272f40ceabe8239
Alejahndro <alejahndro02@gmail.com>
2021-06-30 14:23:26 -0500

    Se importo el modelo y el servicio de empleado para poder obtener la informacion a tra vez del metodo cargarEmpleados que tambine fue creado

--- src/app/components/list-empleado/list-empleado.component.ts
+++ src/app/components/list-empleado/list-empleado.component.ts
@@ -1,7 +1,12 @@
+//Modulos
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+//Modelo
+import { Empleado } from './../../models/empleado';
+//Servicio
+import { EmpleadoService } from './../../services/empleado.service';
 
 
 export interface PeriodicElement {
@@ -31,16 +36,23 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class ListEmpleadoComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
+  listEmpleado: Empleado[];
   @ViewChild(MatPaginator, {static:true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static:true}) sort: MatSort;
-  constructor() { }
+  constructor(private empleadoService: EmpleadoService) { }
 
   ngOnInit(): void {
+    this.cargarEmpleados();
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  cargarEmpleados(){
+    this.listEmpleado =this.empleadoService.getEmpleados();
+    console.log(this.listEmpleado);
+    
   }
 }
