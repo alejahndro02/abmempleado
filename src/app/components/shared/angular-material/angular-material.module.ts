46743ef5a766f7b201fa1656aaf320568891299e
Alejahndro <alejahndro02@gmail.com>
2021-06-30 12:33:36 -0500

    Se importo el modulo de MatFormFieldModule y MatInputModule de AngularMaterial, tambien se importo y se exporto en el @NgModule para poder hacer uso de los comdulos

--- src/app/components/shared/angular-material/angular-material.module.ts
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -4,19 +4,23 @@ import { CommonModule } from '@angular/common';
 //Imports desde Angular material, estos se tinen que importar como  exportar desde el @NgModule
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
-
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
 
-  ], 
+  ],
   exports:[
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AngularMaterialModule { }
