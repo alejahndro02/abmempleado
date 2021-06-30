2974a1371df502301bc212de4d0fe736ab3c5a3d
Alejahndro <alejahndro02@gmail.com>
2021-06-30 11:18:51 -0500

    Se importo el modulo de MatToolbatModule de AngularMaterial, tambien se importo y se exporto en el @NgModule

--- src/app/components/shared/angular-material/angular-material.module.ts
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Imports desde Angular material, estos se tinen que importar como  exportar desde el @NgModule
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+
+  ], 
+  exports:[
+    MatToolbarModule
   ]
 })
 export class AngularMaterialModule { }
