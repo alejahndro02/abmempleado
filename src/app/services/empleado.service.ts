b7ab7c4fde992104dba326e1c32c8fdad27f2ff2
Alejahndro <alejahndro02@gmail.com>
2021-07-04 09:09:48 -0500

    Se declaro el metodo agregarEmpleado

--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -9,25 +9,25 @@ import { Empleado } from './../models/empleado';
 export class EmpleadoService {
   listEmpleado: Empleado[]=[
     { nombreCompleto:'Lucas martinez', telefono:1223331223, correo:'mail@mail.com', 
-    fechaIngreso:new Date(), estadoCivil:'soltero', sexo:'Masculino'},
+    fechaIngreso:new Date(), estadoCivil:'soltero', genero:'Masculino'},
     
     { nombreCompleto:'Lupita Rocilo', telefono:1223331223, correo:'mail@mail.com', 
-    fechaIngreso:new Date(), estadoCivil:'soltero', sexo:'Femenino'},
+    fechaIngreso:new Date(), estadoCivil:'soltero', genero:'Femenino'},
     
     { nombreCompleto:'Jose Hernandez', telefono:2135654633, correo:'mail@mail.com', 
-    fechaIngreso:new Date(), estadoCivil:'soltero', sexo:'Masculino'},
+    fechaIngreso:new Date(), estadoCivil:'soltero', genero:'Masculino'},
     
     { nombreCompleto:'Rosa Abendaño', telefono:1223331223, correo:'mail@mail.com', 
-    fechaIngreso:new Date(), estadoCivil:'soltero', sexo:'Femenino'},
+    fechaIngreso:new Date(), estadoCivil:'soltero', genero:'Femenino'},
     
     { nombreCompleto:'Bread Shon', telefono:2135654633, correo:'mail@mail.com', 
-    fechaIngreso:new Date(), estadoCivil:'soltero', sexo:'Masculino'},
+    fechaIngreso:new Date(), estadoCivil:'soltero', genero:'Masculino'},
     
     { nombreCompleto:'Shan Chong', telefono:1223331223, correo:'mail@mail.com', 
-    fechaIngreso:new Date(), estadoCivil:'soltero', sexo:'Femenino'},
+    fechaIngreso:new Date(), estadoCivil:'soltero', genero:'Femenino'},
     
     { nombreCompleto:'Rob Piwee', telefono:2135654633, correo:'mail@mail.com', 
-    fechaIngreso:new Date(), estadoCivil:'soltero', sexo:'Masculino'},
+    fechaIngreso:new Date(), estadoCivil:'soltero', genero:'Masculino'},
 ]
   constructor() { }
   getEmpleados(){
@@ -35,5 +35,10 @@ export class EmpleadoService {
   }
   eliminarEmpleado(index:number){
     this.listEmpleado.splice(index,1)
+  }
+  agregarEmpleado(empleado:Empleado){
+    this.listEmpleado.unshift(empleado);
+    // console.log(empleado);
+    
   }
 }
