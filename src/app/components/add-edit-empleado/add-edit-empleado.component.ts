e5d31a31f8223eb40270c297682a29048a7f92d8
Alejahndro <alejahndro02@gmail.com>
2021-07-04 09:12:05 -0500

    Se importo el modelo y el servicio de Empleado de igual manera se creo el metodo guardarEmpleado

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
+import { Empleado } from 'src/app/models/empleado';
+import { EmpleadoService } from './../../services/empleado.service';
 
 @Component({
   selector: 'app-add-edit-empleado',
@@ -15,7 +17,8 @@ export class AddEditEmpleadoComponent implements OnInit {
   myForm : FormGroup;
   imagen = '../../../assets/img/crear-usuario.jpg';
   estadosCiviles: any []=['Soltero', 'Casado', 'Divorsiado', 'Viudo']
-  constructor(private fb:FormBuilder) {
+  constructor(private fb:FormBuilder, 
+              private empleadoService: EmpleadoService) {
     this.myForm = this.fb.group({
       nombreCompleto : [''],
       correo : [''],
@@ -28,5 +31,15 @@ export class AddEditEmpleadoComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  guardarEmpleado(){
+    const empleado: Empleado= {
+      nombreCompleto: this.myForm.get('nombreCompleto').value,
+      correo: this.myForm.get('correo').value,
+      fechaIngreso: this.myForm.get('fechaIngreso').value,
+      telefono: this.myForm.get('telefono').value,
+      estadoCivil: this.myForm.get('estadoCivil').value,
+      genero: this.myForm.get('genero').value
+    };
+    this.empleadoService.agregarEmpleado(empleado);
+  };
 }
