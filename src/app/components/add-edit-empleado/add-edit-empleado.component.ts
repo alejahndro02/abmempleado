fc2fe3e2a2b2f25f78dd0d11800e417d8b3d3273
Alejahndro <alejahndro02@gmail.com>
2021-07-03 19:41:57 -0500

    Se importo el modulo MAT-RADIO-DEFAULT-OPTIONS y se garegaron los providers para poder cambiar el color del radiobutton, tambien se declaro la variable estadoCiviles com tipo array

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 
 @Component({
   selector: 'app-add-edit-empleado',
   templateUrl: './add-edit-empleado.component.html',
-  styleUrls: ['./add-edit-empleado.component.css']
+  styleUrls: ['./add-edit-empleado.component.css'],
+  providers: [{
+    provide: MAT_RADIO_DEFAULT_OPTIONS,
+    useValue: { color: 'primary' },
+}]
 })
 export class AddEditEmpleadoComponent implements OnInit {
-
+  imagen = '../../../assets/img/crear-usuario.jpg';
+  estadosCiviles: any []=['Soltero', 'Casado', 'Divorsiado', 'Viudo']
   constructor() { }
 
   ngOnInit(): void {
