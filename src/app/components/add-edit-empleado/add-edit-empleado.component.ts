aecbe60c1f57a83ab77f64eeeba29a2b47c5c7b8
Alejahndro <alejahndro02@gmail.com>
2021-07-04 09:34:37 -0500

    Se agrego el modulo router para poder navegar entrecomponentes y el modulo MatSnackBar de angular material para poder renderizar un mensaje al hacer click en el boton aceptar

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,8 +1,10 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 import { Empleado } from 'src/app/models/empleado';
 import { EmpleadoService } from './../../services/empleado.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-edit-empleado',
@@ -18,7 +20,9 @@ export class AddEditEmpleadoComponent implements OnInit {
   imagen = '../../../assets/img/crear-usuario.jpg';
   estadosCiviles: any []=['Soltero', 'Casado', 'Divorsiado', 'Viudo']
   constructor(private fb:FormBuilder, 
-              private empleadoService: EmpleadoService) {
+              private empleadoService: EmpleadoService,
+              private route: Router,
+              private _snackBar:MatSnackBar) {
     this.myForm = this.fb.group({
       nombreCompleto : [''],
       correo : [''],
@@ -41,5 +45,9 @@ export class AddEditEmpleadoComponent implements OnInit {
       genero: this.myForm.get('genero').value
     };
     this.empleadoService.agregarEmpleado(empleado);
+    this._snackBar.open('El empleado fue registrado con exito', '', {
+      duration:3000
+    });
+    this.route.navigate(['/'])
   };
 }
