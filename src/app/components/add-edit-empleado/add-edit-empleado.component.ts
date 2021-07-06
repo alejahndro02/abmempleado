47750c166663e484016726b7aed1e19bfe6484fe
Alejahndro <alejahndro02@gmail.com>
2021-07-05 21:18:14 -0500

    Se declararon los metodos agregarEmpleado, editarEmpleado y preEditar

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,9 +1,9 @@
-import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Empleado } from 'src/app/models/empleado';
-import { EmpleadoService } from './../../services/empleado.service';
+import { EmpleadoService } from 'src/app/services/empleado.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -13,30 +13,39 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   providers: [{
     provide: MAT_RADIO_DEFAULT_OPTIONS,
     useValue: { color: 'primary' },
-}]
+  }]
 })
 export class AddEditEmpleadoComponent implements OnInit {
+  img ='../../../assets/img/crear-usuario.jpg'
+  estadosCiviles: any []=['Soltero', 'Casado', 'Divorciado', 'Viudo'];
+  idEmpleado:any;
+  action = 'Crear';
   myForm : FormGroup;
-  imagen = '../../../assets/img/crear-usuario.jpg';
-  estadosCiviles: any []=['Soltero', 'Casado', 'Divorsiado', 'Viudo']
-  constructor(private fb:FormBuilder, 
+  constructor(private fb:FormBuilder,
               private empleadoService: EmpleadoService,
               private route: Router,
-              private _snackBar:MatSnackBar) {
+              private snackBar:MatSnackBar,
+              private aRoute: ActivatedRoute ) {
     this.myForm = this.fb.group({
-      nombreCompleto : ['', [Validators.required, Validators.maxLength(20)]],
-      correo : ['', Validators.required, Validators.email],
-      fechaIngreso : ['', Validators.required],
-      telefono : ['', [Validators.required, Validators.maxLength(10)]],
-      estadoCivil : ['', Validators.required],
-      genero : ['', Validators.required],
+      nombreCompleto: ['', [Validators.required, Validators.maxLength(20)]],
+      correo: ['', [Validators.required, Validators.email]],
+      fechaIngreso: ['', [Validators.required]],
+      telefono: ['', [Validators.required]],
+      estadoCivil: ['', [Validators.required]],
+      genero: ['', [Validators.required]],
     });
+    const idParam= 'id';
+    this.idEmpleado = this.aRoute.snapshot.params[idParam];
    }
 
   ngOnInit(): void {
+    if(this.idEmpleado !== undefined){
+      this.action = 'Editar'
+      this.preEditar();
+    }
   }
   guardarEmpleado(){
-    const empleado: Empleado= {
+    const empleado: Empleado = {
       nombreCompleto: this.myForm.get('nombreCompleto').value,
       correo: this.myForm.get('correo').value,
       fechaIngreso: this.myForm.get('fechaIngreso').value,
@@ -44,10 +53,36 @@ export class AddEditEmpleadoComponent implements OnInit {
       estadoCivil: this.myForm.get('estadoCivil').value,
       genero: this.myForm.get('genero').value
     };
+    if(this.idEmpleado !== undefined ){
+      this.editarEmpleado(empleado);
+    }else{
+      this.agregarEmpleado(empleado);
+    };
+  };
+  agregarEmpleado(empleado:Empleado){
     this.empleadoService.agregarEmpleado(empleado);
-    this._snackBar.open('El empleado fue registrado con exito', '', {
+    this.snackBar.open('El empleado fue registrado con exito', '', {
       duration:3000
     });
-    this.route.navigate(['/'])
-  };
+    this.route.navigate(['/']);
+  }
+  editarEmpleado(empleado:Empleado){
+    this.empleadoService.editEmpleado(empleado, this.idEmpleado);
+    this.snackBar.open('El empleado fue actualizado con exito!', '', {
+      duration: 3000
+    });
+    this.route.navigate(['/']);
+  }
+  preEditar(){
+    const empleado: Empleado = this.empleadoService.getEmpleado(this.idEmpleado);
+    console.log(empleado);
+    this.myForm.patchValue({
+      nombreCompleto: empleado.nombreCompleto,
+      correo: empleado.correo,
+      fechaIngreso: empleado.fechaIngreso,
+      telefono: empleado.telefono,
+      estadoCivil: empleado.estadoCivil,
+      genero: empleado.genero,
+    });
+  }
 }
